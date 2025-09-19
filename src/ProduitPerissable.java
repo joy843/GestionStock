f821ab67915dfbd0c995686d7c7bb4bada557fb9
LynJoyce <joyceboyom05@gmail.com>
2025-09-18 23:18:55 -0400

    Update ProduitPerissable.java

--- src/ProduitPerissable.java
+++ src/ProduitPerissable.java
@@ -1,3 +1,28 @@
-public class Produitperissable {
+//Création de la classe ProduitPerissable qui hérite de Produit
+public class Produitperissable extends Produit{
+	
+	//Déclaration d'une variable d'instance
+	private String dateExp;
+	
+	//Déclaration d'un constructeur prenant en compte la date d'expiration
+	public Produitperissable(String id, String name, int qt, double prix, String d) {
+		super(id,name,qt,prix);
+		this.dateExp = d;
+	}
+	
+	//Accesseur
+	public String getDateExp() {
+		return this.dateExp;
+	}
+	
+	//Mutateur
+	public void setDateExp(String d) {
+		this.dateExp = d;
+	}
+	
+	//Méthode toString tenant compte de la date d'expiration
+	public String toString() {
+		return super.toString() + " date exp= " + this.dateExp;
+	}
 
 }
