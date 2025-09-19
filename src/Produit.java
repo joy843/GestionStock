b2c3c4913acf79cbfb679dde85c65506629893d4
LynJoyce <joyceboyom05@gmail.com>
2025-09-18 23:18:21 -0400

    Update Produit.java

--- src/Produit.java
+++ src/Produit.java
@@ -1,3 +1,51 @@
 public class Produit {
+	
+	//Déclaration des variables d'instances
+	private String identifiant;
+	private String nom;
+	private int quantite;
+	private double prixUnitaire;
+	
+	//Conctructeur
+	public Produit(String id, String name, int qt, double prix) {
+		this.identifiant = id;
+		this.nom = name;
+		this.quantite = qt;
+		this.prixUnitaire = prix;
+	}
+	
+	//Accesseurs
+	public String getIdentifiant() {
+		return this.identifiant;
+	}
+	public String getNom() {
+		return this.nom;
+	}
+	public int getQuantite() {
+		return this.quantite;
+	}
+	public double getPrix() {
+		return this.prixUnitaire;
+	}
+	
+	// Mutateurs
+	public void setIdentifiant(String id) {
+		this.identifiant = id;
+	}
+	public void setNom(String name) {
+		this.nom = name;
+	}
+	public void setQuantite(int qt) {
+		this.quantite = qt;
+	}
+	public void setPrix(double p) {
+		this.prixUnitaire = p;
+	}
+	
+	//toString
+	public String toString() {
+		return " - " + this.identifiant + " nom= " + this.nom + " quantite= " + Integer.toString(this.quantite) + " prix= " + Double.toString(this.prixUnitaire);
+	}
+	
 
 }
